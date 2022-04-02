15d132b186b85c0ec7d095370065205beb0d677f
Naima Boukhiar <39013604+naimabouk@users.noreply.github.com>
2022-04-02 04:28:05 +0200

    Update obstacleQlearningMain.ino

--- obstacleQlearningMain.ino
+++ obstacleQlearningMain.ino
@@ -18,47 +18,35 @@ int FLAG;
 //END//
 
 //adding Q learnig values//
-float ALPHA = 0.1;    //LEARNING RATE
-float GAMMA = 0.5;    //DISCOUNT FACTOR
-float EPSILON = 0.90;  //EXPLORATION PARAMETER
-int REWARD;           //REWARD FOR PERFORMING AN ACTION
+float ALPHA = 0.1;    //
+float GAMMA = 0.5;    //
+float EPSILON = 0.90;  //
+int REWARD;           //
 int EPISODES  = 100;
 
-int STATE;                        // CURRENT STATE OF THE ROBOT
-int ACTION = 0;                   //ACTION PERFORMED BY THE ROBOT(0:FORWARD,1:BACKWARD ,2;STOP,3:LEFT)
-float PROB;                       //USED FOR EPSILON DECAY 
-bool ACTION_TAKEN = false;        //THIS VARIABLES TELLS US WHETHER AN ACTION IS TAKEN OR NOT
-int NEXT_STATE;                   // NEXT STATE OF THE ROBOT
-const int STATES = 10;            //NUMBER OF STATES IN ENVIRONMENT 
+int STATE;                        // 
+int ACTION = 0;                   //
+float PROB;                       //
+bool ACTION_TAKEN = false;        //
+int NEXT_STATE;                   // 
+const int STATES = 10;            //
 int ACTIONS[4] = {1,2,3,4};
-const int NUMBER_OF_ACTIONS = 4; //TOTAL WE HAVE 4 ACTION FORWARD,BACKWARD,LEFT AND STOP
+const int NUMBER_OF_ACTIONS = 4; //
 
 
-/*THIS IS THE Q MATRIX OR Q TABLE. THIS IS BASICALLY THE DIARY THAT ROBOT WILL LOOK INTO 
-BEFORE PERFORMING AN ACTION.BASED ON THE ACTION THE ROBOT WILL EARN REWARD AND THE Q VALUE 
-WILL BE UPDATED IN THIS Q TABLE. HERE I HAVE CONISDERED 10 STATES. I HAVE ASSUMED ALL STATES 
-ARE DIFFERENT EVEN THOUGH THEY ARE SAME.BASICALLY OBSTACLE AVOIDING ROBOT CONTAINS ONLY TWO STATES
-i.e: 
-1:WHEN ITS AWAY FROM OBSTACLE 
-2:WHEN ITS NEAR TO THE OBSTACLE
-BUT HERE TO ILLUSTRATE MORE COMPLEX ENVIRONMENT I HAVE ASSUMED THERE ARE 10 DIFFERENT STATES HERE
-EXPECTING SAME/DIFFERENT ACTION.*/
 
-float Q[STATES][NUMBER_OF_ACTIONS] = {{0.0,0.0,0.0,0.0},  //MOST IMPORTANT OF ALL IS THE Q TABLE.
-                                      {0.0,0.0,0.0,0.0},  //IT IS FORMED BY STATES AS ITS  ROWS 
-                                      {0.0,0.0,0.0,0.0},  //AND COLLUMNS AS ITS NUMBER OF ACTIONS
-                                      {0.0,0.0,0.0,0.0},  //INITIALISED TO ZERO IN THE START
-                                      {0.0,0.0,0.0,0.0},  // THIS WILL UPDATED IN THE FUTURE.
+float Q[STATES][NUMBER_OF_ACTIONS] = {{0.0,0.0,0.0,0.0},  
+                                      {0.0,0.0,0.0,0.0},  
+                                      {0.0,0.0,0.0,0.0},  
+                                      {0.0,0.0,0.0,0.0},  
+                                      {0.0,0.0,0.0,0.0},  
                                       {0.0,0.0,0.0,0.0},
                                       {0.0,0.0,0.0,0.0},
                                       {0.0,0.0,0.0,0.0},
                                       {0.0,0.0,0.0,0.0},
                                       {0.0,0.0,0.0,0.0}};
 
-/*THIS IS A REWARD MATRIX OR REWARD TABLE. THIS IS RESPONSIBLE FOR GIVING
-REWARD TO ROBOT FOR PERFORMING PARTICULAR ACTION. IT STORES THE REWARD FOR 
-EACH ACTION TAKEN AT STATE. THE REWARD WILL BE POSITIVE IF THE ACTION 
-PERFORMED IS GOOD AND NEGATIVE IF ACTION YIELDS BAD RESULTS.*/
+
 
 int REWARDS[STATES][NUMBER_OF_ACTIONS] = {{-10,-2,-1,10}, 
                                           {-10,-2,-1,10}, 
@@ -70,13 +58,11 @@ int REWARDS[STATES][NUMBER_OF_ACTIONS] = {{-10,-2,-1,10},
                                           {-10,-2,-1,10},
                                           {-10,-2,-1,10},
                                           {-10,-2,-1,10}};            
-////////////////////////////////////////////END///////////////////////////////////////////////////
 
-////////////////Q LEARNING UPDATE PARAMETERS////////////
 float Q_OLD;
 float Q_NEW;
 float Q_MAX;
-//////////////////////////END//////////////////////////
+
 
 void setup() 
 {
@@ -93,7 +79,7 @@ void setup()
   delay(5000);
 } 
 
-////////////////////////////ROBOT'S FUNCTIONS/////////////////////////////////
+
 void Forward()
 {
   digitalWrite(in1,LOW);
